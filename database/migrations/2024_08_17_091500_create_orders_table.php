ac1710473e39680a3ab1207877dbc4911112b11c
abdelrahmansaad477 <abdelrahmansaad477@gmail.com>
2024-08-18 07:11:22 +0300

    first commit

---
+++ database/migrations/2024_08_17_091500_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('table_id')->constrained('tables');
+            $table->foreignId('reservation_id')->constrained('reservations');
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->foreignId('user_id')->constrained('users');
+            $table->decimal('total');
+            $table->boolean('paid');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
